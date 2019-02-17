264f06ae7836dd5228a5af7af42dffd04f4b752e
Julio Sanchez <jsanchez191980802@gmail.com>
2019-02-17 09:24:15 -0600

    initial commit

---
+++ app.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const mongoose = require("mongoose");
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+mongoose.connect(
+  "mongodb://julio:1sanchez@ds159661.mlab.com:59661/storybook_db",
+  { useNewUrlParser: true },
+  () => {
+    console.log(`Connected to MongoDB`);
+  }
+);
+
+app.get("/", (req, res) => {
+  res.send("Hello from index");
+});
+
+app.listen(port, () => {
+  console.log(`Listening on port... ${port}`);
+});
