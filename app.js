bfe7d35c1a7bc59f2c539037f67f56b8a788e7bb
Julio Sanchez <jsanchez191980802@gmail.com>
2019-02-18 05:03:58 -0600

    adding googleAuth

--- app.js
+++ app.js
@@ -1,7 +1,15 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const passport = require("passport");
+
+//Passport Config
+require("./config/passport")(passport);
+
+//Load Routes
+const auth = require("./routes/auth");
 
 const app = express();
+
 const port = process.env.PORT || 5000;
 
 const keys = require("./config/keys");
@@ -17,6 +25,9 @@ mongoose.connect(
 app.get("/", (req, res) => {
   res.send("Hello from index");
 });
+
+//Use Routes
+app.use("/auth", auth);
 
 app.listen(port, () => {
   console.log(`Listening on port.... ${port}`);
